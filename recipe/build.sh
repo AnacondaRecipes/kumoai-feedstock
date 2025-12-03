0e7d175be23f90998bbb4efa6cb6aaf340cd1538
Andrii Osipov <aosipov@anaconda.com>
2025-12-03 03:01:52 -0800

    refactor

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,9 +23,9 @@ fi
 echo "ARCH: $ARCH ..."
 
 if [ `uname` == Linux ]; then
-    # No sources for linux aarch64 on pypi -> https://pypi.org/project/kumoai/#files
-    # Build linux aarch64 from -> kumoai-2.12.0-py3-none-any.whl
     if [ "$target_platform" == "linux-aarch64" ]; then
+        # No sources for linux aarch64 on pypi -> https://pypi.org/project/kumoai/#files
+        # Build linux aarch64 from -> kumoai-2.12.0-py3-none-any.whl
         WHL_FILE=https://pypi.org/packages/py3/k/kumoai/kumoai-${PKG_VERSION}-py3-none-any.whl
     elif [ "$target_platform" == "linux-64" ]; then
         if [ "$PY_VER" == "3.10" ]; then
@@ -38,8 +38,6 @@ if [ `uname` == Linux ]; then
             WHL_FILE=https://pypi.org/packages/cp313/k/kumoai/kumoai-${PKG_VERSION}-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl
         fi
     fi
-
-    
 fi
 
 $PYTHON -m pip install --no-deps --no-build-isolation -vvv $WHL_FILE
